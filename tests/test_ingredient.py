b62c02845ec9f5e44aa3b1ffe4d912bfcb033870
Ilya Zolotov <zolotovil@mail.ru>
2024-08-06 13:38:17 +0300

    Edit tests

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -4,8 +4,8 @@ from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FI
 
 class TestIngredient:
     @pytest.mark.parametrize("ingredient_type, name, price", [
-        (INGREDIENT_TYPE_SAUCE, "Барбекю", 49.99),
-        (INGREDIENT_TYPE_FILLING, "Мясная", 399.99)
+        (INGREDIENT_TYPE_SAUCE, "hot sauce", 49.99),
+        (INGREDIENT_TYPE_FILLING, "sausage", 399.99)
     ])
     def test_init_method_true(self, ingredient_type, name, price):
         ingredient = Ingredient(ingredient_type, name, price)
@@ -14,18 +14,18 @@ class TestIngredient:
         assert ingredient.price == price
 
     def test_get_price(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Барбекю", 49.99)
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "hot sauce", 49.99)
         assert ingredient.get_price() == 49.99
         assert isinstance(ingredient.get_price(), float)
 
     def test_get_name(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Мясная", 399.99)
-        assert ingredient.get_name() == "Мясная"
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "sausage", 399.99)
+        assert ingredient.get_name() == "sausage"
         assert isinstance(ingredient.get_name(), str)
 
     @pytest.mark.parametrize("ingredient_type, name, price", [
-        (INGREDIENT_TYPE_SAUCE, "Барбекю", 49.99),
-        (INGREDIENT_TYPE_FILLING, "Мясная", 399.99)
+        (INGREDIENT_TYPE_SAUCE, "hot sauce", 49.99),
+        (INGREDIENT_TYPE_FILLING, "sausage", 399.99)
     ])
     def test_get_type(self, ingredient_type, name, price):
         ingredient = Ingredient(ingredient_type, name, price)
